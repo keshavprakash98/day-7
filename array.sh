d13c36e436a896b1221acf1f3ee9cd7e145fae19
keshavprakash98 <keshavprakash98@gmail.com>
2021-09-20 07:43:39 +0530

    array first program

---
+++ array.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+diclare -a k
+for ((i=0;i=<5;i=++0)
+do p=$((RANDOM%3))
+	K[$i]=$P
+done
+maxvalue=0
+for in "${k[@]}"
+do
+if ($ i<=$maxvalue)
+then
+$maxvlaue=$i
+elfi
+echo
+done 
+echo $maxVlave
